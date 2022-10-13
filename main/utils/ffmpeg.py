67457e587b657e0a5bc234caa6702e129b5c538d
Movie4alll-mkv <77849768+Movie4alll-mkv@users.noreply.github.com>
2022-10-13 23:00:22 +0500

    Update ffmpeg.py

--- main/utils/ffmpeg.py
+++ main/utils/ffmpeg.py
@@ -32,26 +32,26 @@ from .. import resolution as r
 from .. import tune as t
 
 
-def get_codec(filepath, channel='v:0'):
+def get_codec(file, channel='v:0'):
     output = subprocess.check_output(['ffprobe', '-v', 'error', '-select_streams', channel,
                                       '-show_entries', 'stream=codec_name,codec_tag_string', '-of',
                                       'default=nokey=1:noprint_wrappers=1', filepath])
     return output.decode('utf-8').split()
 
 
-async def encode(filepath):
-    path, extension = os.path.splitext(filepath)
-    name = path.split('/')
-    output_filepath = encode_dir + name[len(name)-1] + '.mkv'
-    assert(output_filepath != filepath)
+async def encode(file):
+    path, extension = os.splitext(file)
+    name = file.split('/')
+    output_file = encode_dir + name[len(name)-1] + '.mkv'
+    assert(output_file != file)
 
-    if os.path.isfile(output_filepath):
-        print(f'[Encode] [Warning]: "{output_filepath}": file already exists')
+    if os.isfile(output_file):
+        print(f'[Encode] [Warning]: "{output_file}": file already exists')
     else:
-        print('[Encode]: ' + filepath)
+        print('[Encode]: ' + file)
 
     # Codec and Bits
-    codec = '-c:v libx264 -pix_fmt yuv420p'
+    codec = '-c:v copy -pix_fmt yuv420p'
 
     # CRF
     crf = f'-crf {c}'
@@ -72,14 +72,14 @@ async def encode(filepath):
     video_opts = f'-tune {t} -map 0:v? -map_chapters 0 -map_metadata 0'
 
     # Copy Subtitles
-    subs_i = get_codec(filepath, channel='s:0')
+    subs_i = get_codec(channel='s:0')
     if subs_i == []:
         subtitles = ''
     else:
         subtitles = '-c:s copy -map 0:s?'
 
     # Audio
-    a_i = get_codec(filepath, channel='a:0')
+    a_i = get_codec(channel='a:0')
     a = audio
     if a_i == []:
         audio_opts = ''
@@ -109,15 +109,15 @@ async def encode(filepath):
     finish = '-threads 8'
 
     # Finally
-    command = ['ffmpeg', '-y', '-i', filepath]
+    command = ['ffmpeg', '-y', '-i']
     command.extend((codec.split() + preset.split() + video_opts.split() +
                    crf.split() + resolution.split() + subtitles.split() + audio_opts.split() + finish.split()))
-    proc = await asyncio.create_subprocess_exec(*command, output_filepath, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+    proc = await asyncio.create_subprocess_exec(*command, output_file, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
     await proc.communicate()
-    return output_filepath
+    return output_filename
 
 
-def get_thumbnail(in_filename, path, ttl):
+def get_thumbnail(in_filename, ttl):
     out_filename = os.path.join(path, str(time.time()) + ".jpg")
     open(out_filename, 'a').close()
     try:
@@ -130,11 +130,11 @@ def get_thumbnail(in_filename, path, ttl):
         )
         return out_filename
     except ffmpeg.Error as e:
-        return None
+        return out_filename
 
 
-def get_duration(filepath):
-    metadata = extractMetadata(createParser(filepath))
+def get_duration(filename):
+    metadata = extractMetadata(createParser())
     if metadata.has("duration"):
         return metadata.get('duration').seconds
     else:
